10dea9b622e04e699f353084b6f9a7c0dfed5ec8
Sayyed Sohel Ahmed <sayyedsohel73976@gmail.com>
2024-07-17 16:42:33 +0530

    convert class into function in custom api error and response

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,3 +1,30 @@
+function ApiError(
+  statusCode,
+  message = "Something went wrong",
+  errors = [],
+  stack = ""
+) {
+  const apiError = new Error(message);
+  apiError.statusCode = statusCode;
+  apiError.data = null;
+  apiError.message = message;
+  apiError.success = false;
+  apiError.errors = errors;
+
+  if (stack) {
+    apiError.stack = stack;
+  } else {
+    Error.captureStackTrace(apiError, ApiError);
+  }
+
+  return apiError;
+}
+
+export { ApiError };
+
+/*
+// we can use class as well
+
 class ApiError extends Error {
     constructor(
         statusCode,
@@ -20,5 +47,6 @@ class ApiError extends Error {
 
     }
 }
+export { ApiError };
+*/
 
-export {ApiError}
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,23 @@
-class ApiResponse {
-    constructor(statusCode, data, message = "Success"){
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
-    }
+function ApiResponse(statusCode, data, message = "Success") {
+  return {
+    statusCode: statusCode,
+    data: data,
+    message: message,
+    success: statusCode < 400,
+  };
 }
 
-export { ApiResponse }
+export { ApiResponse };
+
+// we can use class as well
+
+// class ApiResponse {
+//     constructor(statusCode, data, message = "Success"){
+//         this.statusCode = statusCode
+//         this.data = data
+//         this.message = message
+//         this.success = statusCode < 400
+//     }
+// }
+
+// export { ApiResponse }
